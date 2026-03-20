e0783f46e72d1c7256b3915f098df48e2657b143
ASapAbro <143710790+ASapAbro@users.noreply.github.com>
2026-03-20 15:14:39 +0100

    auth backend validé (register, login, logout, refresh, me)

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -35,10 +35,9 @@ const userSchema = new mongoose.Schema(
 )
 
 // Hash le mot de passe avant chaque save
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return
   this.password = await bcrypt.hash(this.password, 12)
-  next()
 })
 
 // Méthode pour comparer un mot de passe entrant avec le hash
